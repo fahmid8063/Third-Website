ecb801fe2d4d92d0c2382854a5461751b66cb8cf
Khondoker Fahmiduzzaman <75840651+fahmid8063@users.noreply.github.com>
2021-03-20 15:22:14 +0600

    Add files via upload

---
+++ Gunoboti/asset/script.js
@@ -0,0 +1,13 @@
+function multiple(){
+
+
+
+	let number1=document.querySelector(".input").value
+	let number2= 270;
+	
+	 
+	let y= parseInt(number1) * parseInt(number2); 
+	document.querySelector("#result").innerHTML=y
+
+
+}
